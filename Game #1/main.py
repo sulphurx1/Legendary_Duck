6f5bb6b1b81446d3036271219acb3d9487472dc6
sulphurx1 <122863442+sulphurx1@users.noreply.github.com>
2023-03-05 13:58:30 +0400

    bug-stuck-in-wall

---
+++ Game #1/main.py
@@ -0,0 +1,47 @@
+import pygame as py
+import sys
+from settings import *
+from map import *
+from player import *
+
+
+
+class Game:
+    def __init__(self):
+        py.init()
+        self.screen = py.display.set_mode(RES)
+        self.clock = py.time.Clock()
+        self.delta_time = 1
+        self.new_game()
+
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+
+    def update(self):
+        self.player.update()
+        py.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        py.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+        self.map.draw()
+        self.player.draw()
+
+    def check_event(self):
+        for event in py.event.get():
+            if event.type == py.QUIT or (event.type == py.KEYDOWN and event.key == py.K_ESCAPE):
+                py.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_event()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ Game #1/settings.py
@@ -0,0 +1,8 @@
+# game settins
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 0
+
+PLAYER_POS = 1.5, 5 # mini_map
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
