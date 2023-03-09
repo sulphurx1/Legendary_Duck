57248947501fde9afa6e636d4f4eb47235eeec07
sulphurx1 <122863442+sulphurx1@users.noreply.github.com>
2023-03-09 13:46:57 +0400

    changes

--- Replica Hackathon_2/web_data/transfer.py
+++ Replica Hackathon_2/web_data/transfer.py
@@ -5,13 +5,7 @@ connection = psy.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname
 
 cursor = connection.cursor()
 
-cursor.execute("SELECT title, description, image_url FROM news")
-
-results = cursor.fetchall()
-
-for row in results:
-    cursor.execute('''INSERT INTO name_of_app_news (title, description, image_url)
-                   VALUES(%s, %s, %s)''',  (row[0], row[1], row[2]))
+cursor.execute("INSERT INTO name_of_app_news SELECT title, description, image_url FROM news")
     
 connection.commit()
 
