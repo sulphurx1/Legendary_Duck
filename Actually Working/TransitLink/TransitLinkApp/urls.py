9eb93e59aa3b44388e280555982469111ce44a9d
sulphurx1 <122863442+sulphurx1@users.noreply.github.com>
2023-04-14 20:02:52 +0400

    deleted

--- Actually Working/TransitLink/TransitLinkApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', home_page, name='home_page'),
-    path('customer', CustomerSignUpView.as_view(), name='customer_signup'),
-    path('customer', StaffSignUpView.as_view(), name='staff_signup'),
-    path('customer', AdministrativeSignUpView.as_view(), name='administrative_signup'),
-]
--- Maybe_this_work/TransitLink/TransitApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Customer, Staff
-
-admin.site.register(User)
-admin.site.register(Customer)
-admin.site.register(Staff)
